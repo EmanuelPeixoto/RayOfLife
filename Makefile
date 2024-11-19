f1964d97a11e01661c1b34dc634ec388ae00e592
MintzyG <ericbraga2000@gmail.com>
2024-11-19 16:04:16 -0300

    Test project on main

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 build:
-	gcc -o Ray-of-Life main.c -lraylib
+	gcc -o Ray-of-Life main.c -lraylib -lm
 test:
-	gcc -o Test-of-Life example.c -lraylib
+	gcc -o Test-of-Life example.c -lraylib -lm
--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "raylib.h"
 
 int main() {
-
+  int width = 1920;
+  int height = 1080;
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetConfigFlags(FLAG_FULLSCREEN_MODE);
+  SetTargetFPS(60);
+  InitWindow(width, height, "Ray-of-Life");
+  while (!WindowShouldClose()) { 
+    BeginDrawing();
+      for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+          DrawPixel(i, j, (Color){ 
+            .r = (unsigned char)(((((float)(i + j) / (width + height)) * 255)-255)*-1),
+            .g = (unsigned char)(((((float)(i + j) / (width + height)) * 255)-255)*-1),
+            .b = (unsigned char)(((((float)(i + j) / (width + height)) * 255)-255)*-1),
+            .a = 255
+          });
+        }
+      }
+      DrawFPS(20, 20);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
 }
