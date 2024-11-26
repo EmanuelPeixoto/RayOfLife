a2fb5a6a8d721e289c4d0232b1341912d52e1fe4
MintzyG <ericbraga2000@gmail.com>
2024-11-26 13:27:55 -0300

    Remember last used menu

--- src/menu_test.c
+++ src/menu_test.c
@@ -8,55 +8,65 @@
 #define MAX_BUTTONS 5
 
 typedef struct {
-    Rectangle rect;
-    const char *label;
+  Rectangle rect;
+  const char *label;
 } Button;
 
+typedef enum {
+  MENU_NONE,
+  MENU_PAUSE,
+  MENU_SETTINGS,
+  MENU_DEBUG
+} MenuState;
+
 typedef struct {
-    Button buttons[MAX_BUTTONS];
-    int buttonCount;
-    float verticalSpacing;
+  Button buttons[MAX_BUTTONS];
+  int buttonCount;
+  float verticalSpacing;
 } Menu;
 
 Menu CreateMenu(const char *labels[], int buttonCount, int screenWidth, int screenHeight) {
-    Menu menu = { .buttonCount = buttonCount, .verticalSpacing = 10.0f };
-    float buttonWidth = 200;
-    float buttonHeight = 40;
+  Menu menu = { .buttonCount = buttonCount, .verticalSpacing = 10.0f };
+  float buttonWidth = 200;
+  float buttonHeight = 40;
 
-    for (int i = 0; i < buttonCount; i++) {
-        menu.buttons[i].rect = (Rectangle){
-            (float)(screenWidth) / 2 - buttonWidth / 2,
-            (float)(screenHeight) / 2 - ((buttonCount * buttonHeight) + ((buttonCount - 1) * menu.verticalSpacing)) / 2 + i * (buttonHeight + menu.verticalSpacing),
-            buttonWidth,
-            buttonHeight,
-        };
-        menu.buttons[i].label = labels[i];
-    }
+  for (int i = 0; i < buttonCount; i++) {
+    menu.buttons[i].rect = (Rectangle){
+      (float)(screenWidth) / 2 - buttonWidth / 2,
+      (float)(screenHeight) / 2 - ((buttonCount * buttonHeight) + ((buttonCount - 1) * menu.verticalSpacing)) / 2 + i * (buttonHeight + menu.verticalSpacing),
+      buttonWidth,
+      buttonHeight,
+    };
+    menu.buttons[i].label = labels[i];
+  }
 
-    return menu;
+  return menu;
 }
 
 int DrawMenu(Menu menu) {
-    for (int i = 0; i < menu.buttonCount; i++) {
-        DrawRectangleRec(menu.buttons[i].rect, Fade(LIGHTGRAY, 0.8f));
-        DrawRectangleLinesEx(menu.buttons[i].rect, 2, BLACK);
-        DrawText(menu.buttons[i].label,
-                 menu.buttons[i].rect.x + (menu.buttons[i].rect.width - MeasureText(menu.buttons[i].label, 20)) / 2,
-                 menu.buttons[i].rect.y + (menu.buttons[i].rect.height - 20) / 2,
-                 20, BLACK);
+  for (int i = 0; i < menu.buttonCount; i++) {
+    DrawRectangleRec(menu.buttons[i].rect, Fade(LIGHTGRAY, 0.8f));
+    DrawRectangleLinesEx(menu.buttons[i].rect, 2, BLACK);
+    DrawText(menu.buttons[i].label,
+             menu.buttons[i].rect.x + (menu.buttons[i].rect.width - MeasureText(menu.buttons[i].label, 20)) / 2,
+             menu.buttons[i].rect.y + (menu.buttons[i].rect.height - 20) / 2,
+             20, BLACK);
 
-        if (CheckCollisionPointRec(GetMousePosition(), menu.buttons[i].rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            return i; // Return the index of the button clicked
-        }
+    if (CheckCollisionPointRec(GetMousePosition(), menu.buttons[i].rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      return i; // Return the index of the button clicked
     }
-    return -1; // No button clicked
+  }
+  return -1; // No button clicked
 }
 
 typedef struct Config {
   bool is_paused;
   bool drawLines;
-  bool showSettingsMenu;
   bool closeApp;
+  bool debugText;
+  bool debugGrid;
+  MenuState currentMenu;
+  MenuState lastMenu;
 } Config;
 
 void draw_grid(Camera2D camera, int screenWidth, int screenHeight, Config cfg) {
@@ -68,8 +78,10 @@ void draw_grid(Camera2D camera, int screenWidth, int screenHeight, Config cfg) {
     screenHeight / camera.zoom
   });
 
-  DrawCircleV(Vector2Add(topLeft, (Vector2){ 50, 50 }), 20, RED);
-  DrawCircleV(Vector2Add(bottomRight, (Vector2){ -50, -50 }), 20, RED);
+  if (cfg.debugGrid) {
+    DrawCircleV(Vector2Add(topLeft, (Vector2){ 50, 50 }), 20, RED);
+    DrawCircleV(Vector2Add(bottomRight, (Vector2){ -50, -50 }), 20, RED);
+  }
 
   float startX = topLeft.x + baseGridSize;
   float startY = topLeft.y + baseGridSize;
@@ -77,7 +89,9 @@ void draw_grid(Camera2D camera, int screenWidth, int screenHeight, Config cfg) {
   startX = floor(startX / baseGridSize) * baseGridSize;
   startY = floor(startY / baseGridSize) * baseGridSize;
 
-  DrawCircleV((Vector2){ startX, startY }, 10, BLUE);
+  if (cfg.debugGrid) {
+    DrawCircleV((Vector2){ startX, startY }, 10, BLUE);
+  }
 
   for (float y = startY; y <= bottomRight.y + 1; y += baseGridSize) {
     Vector2 start = (Vector2){topLeft.x + 1, y};
@@ -112,9 +126,18 @@ void draw_grid(Camera2D camera, int screenWidth, int screenHeight, Config cfg) {
 
 void HandleControls(Config *cfg, Camera2D *camera) {
   if (IsKeyPressed(KEY_ONE)) { cfg->drawLines = !cfg->drawLines; }
-    if (IsKeyPressed(KEY_ESCAPE)) {
-      cfg->showSettingsMenu ? (cfg->showSettingsMenu = false) : (cfg->is_paused = !cfg->is_paused);
+  if (IsKeyPressed(KEY_ESCAPE)) { 
+    if (cfg->is_paused) {
+      cfg->lastMenu = cfg->currentMenu;
+      cfg->is_paused = !cfg->is_paused; 
+    } else if (!cfg->is_paused && cfg->lastMenu == MENU_NONE) {
+      cfg->is_paused = !cfg->is_paused; 
+      cfg->currentMenu = MENU_PAUSE;
+    } else {
+      cfg->is_paused = !cfg->is_paused;
+      cfg->currentMenu = cfg->lastMenu;
     }
+  }
 
   if (!cfg->is_paused) {
     float wheel = GetMouseWheelMove();
@@ -142,13 +165,16 @@ int main() {
   camera.zoom = 1.0f;
   SetTargetFPS(60);
 
-  Config cfg = { false, false, false };
+  Config cfg = { false, false, false, MENU_NONE };
 
-  const char *pauseLabels[] = { "Resume", "Settings", "Quit" };
-  Menu pauseMenu = CreateMenu(pauseLabels, 3, screenWidth, screenHeight);
+  const char *pauseLabels[] = { "Resume", "Settings", "Debug", "Quit" };
+  Menu pauseMenu = CreateMenu(pauseLabels, 4, screenWidth, screenHeight);
 
   const char *settingsLabels[] = { "Toggle Lines", "Return" };
   Menu settingsMenu = CreateMenu(settingsLabels, 2, screenWidth, screenHeight);
+
+  const char *debugLabels[] = { "Grid Markers", "Debug Text", "Return" };
+  Menu debugMenu = CreateMenu(debugLabels, 3, screenWidth, screenHeight);
 
   while (!WindowShouldClose()) {
     HandleControls(&cfg, &camera);
@@ -163,26 +189,40 @@ int main() {
       } EndMode2D();
 
       /*Always Draw*/ {
-        DrawText(TextFormat("Camera Target X: %.2f", camera.target.x), 10, 10, 20, BLACK);
-        DrawText(TextFormat("Camera Target Y: %.2f", camera.target.y), 10, 40, 20, BLACK);
-        DrawText(TextFormat("Camera Zoom: %.2f", camera.zoom), 10, 70, 20, BLACK);
-        DrawText(TextFormat("Paused: %s", cfg.is_paused ? "true" : "false"), 10, 100, 20, BLACK);
+        if (cfg.debugText) {
+          DrawText(TextFormat("Camera Target X: %.2f", camera.target.x), 10, 10, 20, BLACK);
+          DrawText(TextFormat("Camera Target Y: %.2f", camera.target.y), 10, 40, 20, BLACK);
+          DrawText(TextFormat("Camera Zoom: %.2f", camera.zoom), 10, 70, 20, BLACK);
+          DrawText(TextFormat("Paused: %s", cfg.is_paused ? "true" : "false"), 10, 100, 20, BLACK);
+        }
       }
 
       // Draw exclusively to the pause menu
       if (cfg.is_paused) {
         DrawRectangle(0, 0, screenWidth, screenHeight, Fade(BLACK, 0.6f));
-        int clicked = cfg.showSettingsMenu ? DrawMenu(settingsMenu) : DrawMenu(pauseMenu);
+        int clicked = -1;
 
-        if (!cfg.showSettingsMenu) {
-          switch (clicked) {
-            case 0: cfg.is_paused = false; break;       // Resume
-            case 1: cfg.showSettingsMenu = true; break; // Settings
-            case 2: cfg.closeApp = true; break;         // Quit
-          }
-        } else {
-          if (clicked == 0) { cfg.drawLines = !cfg.drawLines; }  // Toggle Lines
-          if (clicked == 1) { cfg.showSettingsMenu = false; }    // Return
+        switch (cfg.currentMenu) {
+          case MENU_PAUSE:
+            switch (DrawMenu(pauseMenu)) {
+              case 0: cfg.is_paused = false; break;
+              case 1: cfg.currentMenu = MENU_SETTINGS; break;
+              case 2: cfg.currentMenu = MENU_DEBUG; break;
+              case 3: cfg.closeApp = true; break;
+            } break;
+          case MENU_SETTINGS:
+            switch (DrawMenu(settingsMenu)) {
+              case 0: cfg.drawLines = !cfg.drawLines; break;
+              case 1: cfg.currentMenu = MENU_PAUSE; break;
+            } break;
+          case MENU_DEBUG:
+            switch (DrawMenu(debugMenu)) {
+              case 0: cfg.debugGrid = !cfg.debugGrid; break;
+              case 1: cfg.debugText = !cfg.debugText; break;
+              case 2: cfg.currentMenu = MENU_PAUSE; break;
+            } break;
+          default:
+            break;
         }
       }
 
